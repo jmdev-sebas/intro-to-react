7d2fb2def6cad1f9b5a99bf1fceb4a5aeeb0c42a
jmdev-sebas <sebastian.belulok@junomarkets.com>
2023-11-30 01:03:52 +0800

    alternative of props and state

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,20 @@
-import reactImg from './assets/react-core-concepts.png';
-import componentsImg from './assets/components.png';
-import propsImg from './assets/config.png';
-import jsxImg from './assets/jsx-ui.png';
-import stateImg from './assets/state-mgmt.png';
-
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description = reactDescriptions[genRandomInt(2)];
-
-  return (<header>
-  <img src={reactImg} alt="Stylized atom" />
-  <h1>React Essentials</h1>
-  <p>
-    {description} React concepts you will need for almost any app you are
-    going to build!
-  </p>
-</header>);
-}
-
-function CoreConcept(props) {
-  return (
-    <li>
-      <img src={props.image} alt={props.title} />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
-    </li>
-  );
-}
-
+import { CORE_CONCEPTS } from './data.js';
+import Header from './components/Header.jsx';
+import CoreConcept from './components/CoreConcept';
+import TabButton from './components/TabButton';
+import { useState } from 'react';
+import { EXAMPLES } from './data.js';
 
 function App() {
+  const [ selectedTopic, setSelectedTopic ] = useState('components');
+
+  function handleClick(selectedButton) {
+    // selectedButton => 'components, 'jsx', 'props, 'state'
+    setSelectedTopic(selectedButton);
+}
+
+  console.log("APP COMPONENT EXECUTING");
+
   return (
     <div>
       <Header />
@@ -43,26 +23,30 @@ function App() {
           <h2>Core Concepts</h2>
           <ul>
             <CoreConcept 
-            title="Components" 
-            description="The core UI building block"
-            image={componentsImg}
+            title={CORE_CONCEPTS[0].title}
+            description={CORE_CONCEPTS[0].description}
+            image={CORE_CONCEPTS[0].image}
             />
-            <CoreConcept 
-            title="JSX" 
-            description="Return (potentially dynamic) HTML(ish) code to define the actual markup"
-            image={jsxImg}
-            />
-            <CoreConcept 
-            title="Props" 
-            description="Make components configurable"
-            image={propsImg}
-            />
-            <CoreConcept 
-            title="States" 
-            description="React-managed data"
-            image={stateImg}
-            />
+            <CoreConcept {...CORE_CONCEPTS[1]}/>
+            <CoreConcept {...CORE_CONCEPTS[2]}/>
+            <CoreConcept {...CORE_CONCEPTS[3]}/>
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleClick('components')}>Components</TabButton>
+            <TabButton onSelect={() => handleClick('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleClick('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleClick('state')}>State</TabButton>
+          </menu>
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
